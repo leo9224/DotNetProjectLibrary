257ba8cbb4fee347d68d340b5dd5c359b9443589
leo9224 <leofetter92@gmail.com>
2024-01-29 10:22:59 +0100

    add authentication class

---
+++ DotNetProjectLibrary/Authentication/Authentication.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Mail;
+
+namespace DotNetProjectLibrary.Authentication
+{
+	public static class Authentication
+	{
+		public static bool CheckValidEmail(string email)
+		{
+
+			bool valid = true;
+
+			try
+			{
+				var emailAddress = new MailAddress(email);
+			}
+			catch
+			{
+				valid = false;
+			}
+
+			return valid;
+		}
+
+		public static bool CheckValidPassword(string password)
+		{
+			bool valid = true;
+
+			if (password == string.Empty)
+			{
+				return false;
+			}
+
+			return valid;
+		}
+	}
+}
