2ae5b1f0d20281174226230d72a9b58dcec199a0
leo9224 <leofetter92@gmail.com>
2024-06-04 13:53:29 +0200

    add ticket class

---
+++ DotNetProjectLibrary/Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetProjectLibrary.Models
+{
+    public class Ticket
+    {
+        public int id { get; set; }
+        public required string site_name { get; set; }
+        public required string room_name { get; set; }
+        public required string computer_name {  get; set; }
+        public required string email {  get; set; }
+        public required string message {  get; set; }
+    }
+}
