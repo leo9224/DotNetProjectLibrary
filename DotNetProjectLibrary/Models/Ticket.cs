eda4f4087e9f66a45387d12efe1da61a4d672099
leo9224 <leofetter92@gmail.com>
2024-06-04 15:54:58 +0200

    add created_at and updated_at for Ticket

--- DotNetProjectLibrary/Models/Ticket.cs
+++ DotNetProjectLibrary/Models/Ticket.cs
@@ -3,6 +3,8 @@
     public class Ticket
     {
         public int id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime? updated_at { get; set; }
         public required string site_name { get; set; }
         public required string room_name { get; set; }
         public required string computer_name {  get; set; }
